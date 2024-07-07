e9a876d49750da19920372c5410bd8eef9d0d04b
Jacob Westra <77866005+jbwx@users.noreply.github.com>
2024-07-07 00:08:37 -0400

    Test

--- push.sh
+++ push.sh
@@ -9,7 +9,7 @@ fi
 # Perform git operations
 git add .
 git commit -m "$1"
-if git push -u origin master; then
+if git push -u origin main; then
   # Get the latest commit SHA-1 hash
   SHA1_HASH=$(git rev-parse HEAD)
   # Get the current timestamp
